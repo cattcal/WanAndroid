612daa2d5ddbb2d2b949989a5b0681a6cffc8a98
hujw <hujw_android@163.com>
2020-04-24 11:53:33 +0800

    新增常用网站

--- app/src/main/java/cn/hujw/wanandroid/module/mine/activity/CommonlyUsedWebSiteActivity.java
+++ app/src/main/java/cn/hujw/wanandroid/module/mine/activity/CommonlyUsedWebSiteActivity.java
@@ -1,11 +1,7 @@
 package cn.hujw.wanandroid.module.mine.activity;
 
-import android.os.Bundle;
-import android.view.View;
-
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.google.android.flexbox.FlexDirection;
 import com.google.android.flexbox.FlexWrap;
 import com.google.android.flexbox.FlexboxLayoutManager;
